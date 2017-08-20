e1b8956e7558c42736466ca376621c17955e70a9
975168367@qq.com <975168367@qq.com>
2017-08-20 23:04:22 +0800

    redis集群客户端 python版本

---
+++ redis/redisCli.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+# 使用与redis集群的操作
+from rediscluster import StrictRedisCluster
+import sys
+
+
+def redis_cluster():
+    redis_nodes = [{'host': '192.168.1.113', 'port': 7005},
+                   {'host': '192.168.1.113', 'port': 7004},
+                   {'host': '192.168.1.113', 'port': 7003},
+                   {'host': '192.168.1.113', 'port': 7000},
+                   {'host': '192.168.1.113', 'port': 7001},
+                   {'host': '192.168.1.113', 'port': 7002}
+                   ]
+    try:
+        redisconn = StrictRedisCluster(startup_nodes=redis_nodes)
+    except Exception, e:
+        print "connect error"
+        sys.exit(1)
+
+    redisconn.set('name', 'kk')
+    print "name is", redisconn.get('name')
+    print(help(redisconn))
+
+redis_cluster()
